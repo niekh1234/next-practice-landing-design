ffd6acb38133c05aed11043f467512334f711320
Niek Hagen <niekhagen@hotmail.com>
2021-04-22 14:10:33 +0200

    remove fade from mobile nav

--- components/Layout/MobileNav.tsx
+++ components/Layout/MobileNav.tsx
@@ -27,32 +27,30 @@ const MobileNav = (props: Props) => {
   ));
 
   return (
-    <Fade duration={200} className='fixed inset-0 z-50 w-screen h-screen'>
-      <div className='w-full h-full bg-teal-lightest'>
-        <div className='flex items-center justify-between h-20 p-4'>
-          <Link href='/'>
-            <a className='text-2xl font-bold border-b-2 border-pink'>yourcompany.com</a>
-          </Link>
-          <button onClick={closeButtonClickHandler} className='p-2'>
-            <svg
-              className='w-8 h-8'
-              fill='none'
-              stroke='currentColor'
-              viewBox='0 0 24 24'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth='2'
-                d='M6 18L18 6M6 6l12 12'
-              ></path>
-            </svg>
-          </button>
-        </div>
-        <nav className='flex flex-col justify-center w-full mt-32 '>{nav}</nav>
+    <div className='fixed inset-0 z-50 w-screen h-screen bg-teal-lightest'>
+      <div className='flex items-center justify-between h-20 p-4'>
+        <Link href='/'>
+          <a className='text-2xl font-bold border-b-2 border-pink'>yourcompany.com</a>
+        </Link>
+        <button onClick={closeButtonClickHandler} className='p-2'>
+          <svg
+            className='w-8 h-8'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M6 18L18 6M6 6l12 12'
+            ></path>
+          </svg>
+        </button>
       </div>
-    </Fade>
+      <nav className='flex flex-col justify-center w-full mt-32 '>{nav}</nav>
+    </div>
   );
 };
 
